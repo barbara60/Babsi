97ab1bdc7ca133879d53fac1bf2ad60dc0fbf316
Schedl <nero100@gmx.net>
2017-05-01 11:29:34 +0200

    Added comment main.java

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		//Hier eine Aenderung
+		//um Git zu testen
 		// öffnet ein AWT-Fenster
 		MaxCorrelationsKlassifikator max = new MaxCorrelationsKlassifikator();
 		
